13dfc92ada865e80fb6a60ab92d3b66241220f2a
Rishabh Singhal <60057118+rishabh090900@users.noreply.github.com>
2023-04-19 12:30:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Template1 from './components/template';
+function App() {
+  return (
+    <div className="App">
+      Resume Builder
+      <Template1/>  
+    </div>
+
+  );
+}
+
+export default App;
